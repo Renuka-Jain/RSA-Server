ed744aeb29a69b9a8f970b6813db9afc2c38c940
Renuka-Jain <renuka.jain@estudiantat.upc.edu>
2023-05-03 00:13:32 +0200

    falta verificar i desencryptar

--- app.ts
+++ app.ts
@@ -2,41 +2,50 @@ import express from 'express'
 import cors from 'cors'
 import * as rsa from './rsa'
 import * as bigintConversion from 'bigint-conversion'
+import { Request, Response } from 'express';
+import { Router } from 'express';
+import bodyParser from "body-parser";
 
+const router = Router();
 
 const port = 3001
-
 
 const app= express()
 app.use(cors({
     origin: 'http://localhost:3000' //url del cliente
 }))
 
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json())
+
 app.get('/',(req,res)=>{
     res.send('hello arnauuu')
-
 })
 
+app.post('/signed', async(req: Request, res: Response)=>{
+    const text = req.body;
+    console.log(text)
+    //text esta signat, falta verificar
+})
 
-app.get('/rsa',async(req,res)=>{
+app.post('/encrypted', async(req: Request, res: Response)=>{
+    console.log(req.body.text)
+    //text esta encryptat, falta despencriptar
+})
+
+app.get('/rsapubkeyserver',async(req,res)=>{ //desde el cliente se puede pedir public key
     const rsaKeys = await rsa.generateKeys(2048)   
     console.log(rsaKeys)   
     //res.json({publicKey: rsaKeys.publicKey})
     res.json({publickey: {
         e: bigintConversion.bigintToHex(rsaKeys.publicKey.e),
         n: bigintConversion.bigintToHex(rsaKeys.publicKey.n)
-
     }})
 })
 
-app.get('/user',(req,res)=>{
-    const user = {
-        username:'arnauuuu',
-        description: 'es inteligenteeee'
 
-    }
-    res.json(user)
-})
+
+
 
 app.listen(port, function() {
     console.log(`listenning on http://locahost:${port}`)
